b538e5123ebb1ca63f1a7de25b6c6fdb89bddc1d
Vinh Nguyen <vinh.nguyen@v-key.com>
2021-03-19 13:13:33 +0700

    Modify README for environment switching

--- src/test/java/tests/SampleTest.java
+++ src/test/java/tests/SampleTest.java
@@ -1,14 +1,26 @@
 package tests;
 
 import annotations.TestInfo;
+import businessintents.Home;
+import data.Token;
+import data.TokenFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.testng.annotations.Test;
+import utils.LogUtils;
 
 @TestInfo(module = "sample",
 		priority = TestInfo.Priority.MEDIUM,
 		createdBy = "vinh.nguyen")
+
 public class SampleTest extends BaseTest {
+	@Value("${url}")
+	private String url;
+
 	@Test(description = "Verify application is installed")
 	public void verifyApplicationIsInstalled() {
-		verificationKw.verifyEqual(applicationKw.isAppInstalled(applicationKw.getCurrentPackage()), true);
+		LogUtils.info(url);
+		Token token = TokenFactory.createPkiToken();
+		Home.dontSetTiAlert();
+		Home.provisionToken(token.getTokenSerial(), token.getaPin());
 	}
 }
