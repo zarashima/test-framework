c58117495231e11378c078d4d9916c51ba60b0c0
Vinh Nguyen <vinh.nguyen@v-key.com>
2021-03-19 13:19:02 +0700

    Revert sample test changes

--- src/test/java/tests/SampleTest.java
+++ src/test/java/tests/SampleTest.java
@@ -1,26 +1,17 @@
 package tests;
 
 import annotations.TestInfo;
-import businessintents.Home;
-import data.Token;
-import data.TokenFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.testng.annotations.Test;
-import utils.LogUtils;
 
 @TestInfo(module = "sample",
 		priority = TestInfo.Priority.MEDIUM,
 		createdBy = "vinh.nguyen")
-
 public class SampleTest extends BaseTest {
-	@Value("${url}")
-	private String url;
-
 	@Test(description = "Verify application is installed")
 	public void verifyApplicationIsInstalled() {
-		LogUtils.info(url);
-		Token token = TokenFactory.createPkiToken();
-		Home.dontSetTiAlert();
-		Home.provisionToken(token.getTokenSerial(), token.getaPin());
+		verificationKw.verifyEqual(applicationKw.isAppInstalled(
+				applicationKw.getCurrentPackage()), true);
+		verificationKw.verifyEqual(applicationKw.isAppInstalled(applicationKw.getCurrentPackage()), true);
 	}
 }
+
